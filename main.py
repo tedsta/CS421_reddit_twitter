3c7a3227b454a80e9b933f5973b23b55d0fde0d3
Theodore DeRego <tderego94@gmail.com>
2015-05-04 10:41:54 -1000

    First query working

--- main.py
+++ main.py
@@ -58,43 +58,46 @@ def build_database(db):
             "FOREIGN KEY(word) REFERENCES word(word)"\
         ");"
     )
-    
-    db.commit()
+
+####################################################################################################
 
 def populate_word(db, words):
     for word in words:
-        db.execute("INSERT INTO word(?, ?);", word)
-    db.commit()
+        db.execute("INSERT INTO word values (?, ?);", word)
 
 def populate_user(db, users):
     for user in users:
-        db.execute("INSERT INTO user(?, ?);", user)
-    db.commit()
+        db.execute("INSERT INTO user values (?, ?);", user)
 
 def populate_post(db, posts):
     for post in posts:
-        db.execute("INSERT INTO post(?, ?, ?, ?);", post)
-    db.commit()
+        db.execute("INSERT INTO post values (?, ?, ?, ?);", post)
 
 def populate_reddit_post(db, reddit_posts):
     for reddit_post in reddit_posts:
-        db.execute("INSERT INTO reddit_post(?, ?);", reddit_post)
-    db.commit()
+        db.execute("INSERT INTO reddit_post values (?, ?);", reddit_post)
 
 def populate_twitter_post(db, twitter_posts):
     for twitter_post in twitter_posts:
-        db.execute("INSERT INTO twitter_post(?, ?);", twitter_post)
-    db.commit()
+        db.execute("INSERT INTO twitter_post values (?, ?);", twitter_post)
 
 def populate_user_word(db, user_words):
     for user_word in user_words:
-        db.execute("INSERT INTO user_word(?, ?);", user_word)
-    db.commit()
+        db.execute("INSERT INTO user_word values (?, ?);", user_word)
 
 def populate_post_word(db, post_words):
     for post_word in post_words:
-        db.execute("INSERT INTO post_word(?, ?);", post_word)
-    db.commit()
+        db.execute("INSERT INTO post_word values (?, ?);", post_word)
+
+####################################################################################################
+
+def reddit_posts_from_user(db, user):
+    db.execute('SELECT content '\
+               'FROM post '\
+               'WHERE post.author = ?;', (user,))
+    return [tuple(row) for row in db.fetchall()]
+
+####################################################################################################
 
 def main():
     # Data setup
@@ -105,7 +108,11 @@ def main():
     r = praw.Reddit(user_agent=user_agent)
 
     subreddit = r.get_subreddit('rust')
-    for submission in subreddit.get_hot(limit=50):
+    for submission in subreddit.get_hot(limit=500):
+        if not submission.author:
+            print("Skipping post with no author: "+submission.title.lower())
+            continue
+    
         id = submission.id
         title = submission.title.lower()
         subreddit = 'rust'
@@ -144,7 +151,7 @@ def main():
     posts = [(post[0], post[3], post[1], post[4]) for post in post_data]
     reddit_posts = [(post[0], post[2]) for post in post_data]
     
-    with open("users.txt", "w") as file:
+    """with open("users.txt", "w") as file:
         file.write("\n".join([" ".join(list(user)) for user in users]))
     with open("words.txt", "w") as file:
         file.write("\n".join([" ".join([word, str(use_count)]) for word, use_count in words.items()]))
@@ -157,7 +164,7 @@ def main():
     with open("reddit_posts.txt", "w") as file:
         file.write("\n".join([" ".join(list(reddit_post)) for reddit_post in reddit_posts]))
     
-    exit()
+    exit()"""
     
     # Connect to sqlite
     db = sqlite3.connect(':memory:')
@@ -167,10 +174,19 @@ def main():
     
     build_database(db_cur)
     
+    populate_word(db_cur, [(word, use_count) for word, use_count in words.items()])
+    populate_user(db_cur, users)
+    populate_post(db_cur, posts)
+    populate_reddit_post(db_cur, reddit_posts)
+    populate_post_word(db_cur, post_word)
+    populate_user_word(db_cur, user_word)
+    
+    print("\n".join([str(row) for row in reddit_posts_from_user(db_cur, "brson")]))
+    
+    db.commit()
+    
     # Close DB connection
     db.close()
-    
-    
 
 ############################################
 
